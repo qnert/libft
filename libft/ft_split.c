737c39724a719bc5e4b6480cf2d1bbcd0ceff668
Simon Kunert <skunert@1-A-23.42heilbronn.de>
2023-03-19 19:15:17 +0100

    Norminette approved

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,11 +6,43 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 18:51:00 by skunert           #+#    #+#             */
-/*   Updated: 2023/03/16 19:11:09 by skunert          ###   ########.fr       */
+/*   Updated: 2023/03/19 19:14:06 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	count_splits(char *s, char c)
+//#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static int	check_split_words(const char *s, char c)
+{
+	int	i;
+	int	counter;
+
+	i = 0;
+	counter = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			if (s[i + 1] == c && s[i + 1] != '\0')
+			{
+			}
+			else
+			{
+				counter++;
+			}
+		}
+		i++;
+	}
+	if (s[ft_strlen((char *)s) - 1] == c)
+	{
+		counter--;
+	}
+	return (counter + 1);
+}
+
+static int	check_split_len(const char *s, char c)
 {
 	int	i;
 
@@ -18,11 +50,71 @@ static int	count_splits(char *s, char c)
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
-			i++;
+			break;
+		i++;
 	}
 	return (i);
 }
 
+static char	*get_word(char *s, char c)
+{
+	int		i;
+	int		tmp;
+	char	*str;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			break;
+		i++;
+	}
+	str = ft_calloc(i + 1, sizeof(char));
+	tmp = i;
+	i = 0;
+	while (i < tmp)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+static char	*get_next_word(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			while(s[i] != '\0' && s[i] == c)
+			{
+				i++;
+			}
+			break;
+		}
+		i++;
+	}
+	return (s + i);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	int	i;
+	char **arr;
+
+	return (NULL);
+}
+
+int	main(void)
+{
+	char *s = "Simon";
+	char c = 'm';
+	char *str = get_next_word(s, c);
+
+	printf("%s\n", str);
+	return (0);
 }
