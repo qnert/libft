d6a5a3a9a7a43ec76dd7797ca9b72c38ebada3f3
Simon Kunert <skunert@1-A-23.42heilbronn.de>
2023-03-19 16:32:13 +0100

    Norminette approved

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,27 +6,11 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 14:51:08 by skunert           #+#    #+#             */
-/*   Updated: 2023/03/18 22:57:18 by skunert          ###   ########.fr       */
+/*   Updated: 2023/03/19 16:31:49 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include <stdio.h>
-#include <stddef.h>
-
-char	*ft_strchr(const char *str, int c)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == (char)c)
-			return ((char *) str + i);
-		i++;
-	}
-	return (NULL);
-}
+#include "libft.h"
 
 static int	check_front(const char *s, const char *set)
 {
@@ -48,10 +32,10 @@ static int	check_back(const char *s, const char *set)
 	int		len;
 
 	i = 0;
-	len = ft_strlen(s);
-	while (s[len] != s[0])
+	len = ft_strlen((char *)s);
+	while (s[len - 1] != s[0])
 	{
-		if (strchr(set, s[len]) != 0)
+		if (ft_strchr(set, s[len - 1]) == 0)
 			break;
 		i++;
 		len--;
@@ -59,15 +43,14 @@ static int	check_back(const char *s, const char *set)
 	return (i);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
+	int	i;
+	int	start;
+	int	cpy_len;
 
-}
-
-int main(void)
-{
-	char *s = "Simon";
-	char *set = "Sni";
-	printf("%d\n", ft_strtrim(s, set));
-	return (0);
+	i = 0;
+	start = check_front(s1, set);
+	cpy_len = ft_strlen((char *)s1) - check_back(s1, set) - start;
+	return (ft_substr(s1, start, cpy_len));
 }
