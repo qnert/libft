a28ad38ae378a18e92d91abacb1114e3997c2544
Simon Kunert <skunert@1-A-6.42heilbronn.de>
2023-03-20 19:33:17 +0100

    Norminette approved

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 18:29:29 by skunert           #+#    #+#             */
-/*   Updated: 2023/03/20 18:28:56 by skunert          ###   ########.fr       */
+/*   Updated: 2023/03/20 19:30:48 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ void	*ft_calloc(size_t nitems, size_t size)
 	ptr = malloc(nitems * size);
 	if (!ptr)
 		return (NULL);
-	ft_bzero(ptr, nitems);
+	ft_bzero(ptr, nitems * size);
 	return (ptr);
 }
