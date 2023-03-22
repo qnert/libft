ef82b2732d4769c89650e5f980407683fa50fbff
qnert <127817637+qnert@users.noreply.github.com>
2023-03-22 20:13:15 +0100

    Strlcat variable change for better understanding

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,22 +6,22 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:20:03 by skunert           #+#    #+#             */
-/*   Updated: 2023/03/22 20:04:43 by skunert          ###   ########.fr       */
+/*   Updated: 2023/03/22 20:12:20 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t max_dstsize)
 {
 	unsigned int	i;
 	unsigned int	dst_len;
 
 	i = 0;
 	dst_len = ft_strlen(dst);
-	if (dstsize <= dst_len)
-		return (dstsize + ft_strlen((char *)src));
-	while (src[i] != '\0' && dst_len + i < dstsize - 1)
+	if (max_dstsize <= dst_len)
+		return (max_dstsize + ft_strlen((char *)src));
+	while (src[i] != '\0' && dst_len + i < max_dstsize - 1)
 	{
 		dst[dst_len + i] = src[i];
 		i++;
