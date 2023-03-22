f867f8167fc6a5c391edf36e1240e8b11d54e098
qnert <127817637+qnert@users.noreply.github.com>
2023-03-22 20:08:52 +0100

    Final Update strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:20:03 by skunert           #+#    #+#             */
-/*   Updated: 2023/03/22 19:55:30 by skunert          ###   ########.fr       */
+/*   Updated: 2023/03/22 20:04:43 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	dst_len = ft_strlen(dst);
 	if (dstsize <= dst_len)
 		return (dstsize + ft_strlen((char *)src));
-	while (src[i] != '\0' && dst_len + i + 1 < dstsize)
+	while (src[i] != '\0' && dst_len + i < dstsize - 1)
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
 	dst[dst_len + i] = '\0';
-	return (dst_len + ft_strlen((char *)src + i));
+	return (ft_strlen(dst) + ft_strlen((char *)src + i));
 }
