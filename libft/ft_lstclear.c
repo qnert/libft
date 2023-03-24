b420e027e4eb2106d861ad4269ffaf8d0205d705
qnert <127817637+qnert@users.noreply.github.com>
2023-03-24 11:21:26 +0100

    lstclear add but abort fault

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,8 +6,23 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 19:50:19 by skunert           #+#    #+#             */
-/*   Updated: 2023/03/23 21:48:06 by skunert          ###   ########.fr       */
+/*   Updated: 2023/03/24 11:04:52 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// void ft_lstclear(t_list **lst, void (*del)(void*))
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*curr_node;
+	t_list	*tmp_free;
+
+	curr_node = *lst;
+	while (curr_node != NULL)
+	{
+		del(curr_node);
+		tmp_free = curr_node;
+		curr_node = curr_node->next;
+		free(tmp_free);
+	}
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 11:10:29 by skunert           #+#    #+#             */
-/*   Updated: 2023/03/23 21:22:11 by skunert          ###   ########.fr       */
+/*   Updated: 2023/03/24 11:06:01 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ int		ft_isprint(int c);
 char	*ft_itoa(int n);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstlast(t_list *lst);
