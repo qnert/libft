e9bbf1d519de3c2b3596e49e42b7f4d76e74fe9d
Simon Kunert <skunert@1-A-8.42heilbronn.de>
2023-03-24 19:41:15 +0100

    lstmap add and libft.h and makefile update

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/24 19:26:41 by skunert           #+#    #+#             */
+/*   Updated: 2023/03/24 19:40:10 by skunert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
+{
+	t_list	*new_lst;
+
+	new_lst = ft_lstnew(0);
+	if (new_lst == NULL)
+		return (NULL);
+}
